edc3c6e58d6b7213a129f14537f76c2fbe2085a3
Abdel-Kader <kabdel552@gmail.com>
2022-02-10 02:27:47 +0000

    edit config

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/gestion-de-vol-app'));
+app.use(express.static(__dirname + '/dist/gestion-de-vol-app'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/gestion-de-vol-app/'}),
-);
+app.get('/*', function(req,res)
+{
+  res.sendFile(path.join(__dirname + '/dist/gestion-de-vol-app/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
