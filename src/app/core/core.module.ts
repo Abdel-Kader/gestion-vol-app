fb7c0f4466df7a0125b714f4809993b82b6e872d
Abdel-Kader <kabdel552@gmail.com>
2022-02-10 20:53:18 +0000

    Adding routerModule to core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,7 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import { LoaderComponent } from './components/loader/loader.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { LoaderComponent } from './components/loader/loader.component';
         PublicModule,
         HttpClientModule,
         FormsModule,
+        RouterModule,
     ]
 })
 export class CoreModule { }
